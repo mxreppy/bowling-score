2caecad60e47019a9451f6c559a0f1f2b23ce427
Aaron Held <aarondheld@gmail.com>
2014-12-19 10:16:18 -0500

    20 * 1 = 20, not 10

--- test/bowling_test.js
+++ test/bowling_test.js
@@ -1,21 +1,39 @@
 var assert = require("should")
 var gameConstructor = require("../bowl.js").game
 
+function rollMany(game, rolls, pins){
+
+      var i = 0;
+      for (i; i < rolls; i ++) {
+        game.roll(pins);
+      }
+
+}
 describe('Bowling Game', function(){
+
+  beforeEach('Setup a new game', function(){
+    this.game = new gameConstructor();
+    });
+
   describe('Gutter game', function(){
     it('Should have a zero score when no pins are knocked down', function(){
-      game = new gameConstructor();
-      
-      var i = 0;
-      for (i; i < 20; i ++) {
-          game.roll(0);
-      }
-      game.score().should.equal(0);
+      rollMany(this.game, 20, 0);
+      this.game.score().should.equal(0);
     })
   })
+  describe('Simple Games', function(){
+    it("Should score 20 if all 1s are rolled", function(){
 
-  //beforeEach('Setup a new game', function(){
-    //var game = new gameConstructor();
-    //});
+      rollMany(this.game, 20, 1);
+      this.game.score().should.equal(20);
+
+    })
+    it("Should score 40 when all 2s are rolled", function(){
+    rollMany(this.game, 20, 2);
+    this.game.score().should.equal(40);
+    })
+
+  })
+
 
 })
