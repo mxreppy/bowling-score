1d5d5e33efbec92e43c2cef192d58f411b85451b
Aaron Held <aarondheld@gmail.com>
2015-01-08 20:30:33 -0500

    working html testing

--- test/bowling_test.js
+++ test/bowling_test.js
@@ -1,38 +1,23 @@
-var assert = require("should")
-var gameConstructor = require("../bowl.js").game
-
-function rollMany(game, rolls, pins){
-
-      var i = 0;
-      for (i; i < rolls; i ++) {
-        game.roll(pins);
-      }
-
+if (typeof require != 'undefined'){
+    // In node.js
+    var assert = require("should")
+    var gameConstructor = require("../bowl.js").game
+} else {
+    var gameConstructor = game
 }
+
 describe('Bowling Game', function(){
 
   beforeEach('Setup a new game', function(){
-    this.game = new gameConstructor();
+    //this.game = new gameConstructor();
     });
 
   describe('Gutter game', function(){
     it('Should have a zero score when no pins are knocked down', function(){
-      rollMany(this.game, 20, 0);
-      this.game.score().should.equal(0);
+    game = new gameConstructor();
+    for (var i = 0; i < 20; i++) game.roll(0);
+    game.score().should.equal(0);
     })
-  })
-  describe('Simple Games', function(){
-    it("Should score 20 if all 1s are rolled", function(){
-
-      rollMany(this.game, 20, 1);
-      this.game.score().should.equal(20);
-
-    })
-    it("Should score 40 when all 2s are rolled", function(){
-    rollMany(this.game, 20, 2);
-    this.game.score().should.equal(40);
-    })
-
   })
 
 
